21650a77c916471ac47070949c770f80454e1833
Guilherme Pappi <guipappibr@gmail.com>
2025-05-21 09:33:32 -0300

    fix: corrigindo uso de useSearchParams com Suspense

--- src/app/auth/google/callback/page.tsx
+++ src/app/auth/google/callback/page.tsx
@@ -1,37 +1,10 @@
-'use client';
+import { Suspense } from 'react';
+import GoogleCallback from './GoogleCallback';
 
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Cookies from 'js-cookie';
-import { useAuth } from '@/context/AuthContext';
-import api from '@/services/api';
-
-export default function GoogleCallback() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const token = params?.get('token');
-  const { setUser } = useAuth();
-
-  useEffect(() => {
-    const handleLogin = async () => {
-      if (token) {
-        Cookies.set('token', token);
-
-        try {
-          const res = await api.get('/me', {
-            headers: { Authorization: `Bearer ${token}` },
-          });
-          setUser(res.data);
-          router.push('/dashboard');
-        } catch (err) {
-          console.error('Erro ao buscar usuário', err);
-          router.push('/login');
-        }
-      }
-    };
-
-    handleLogin();
-  }, [token, setUser, router]);
-
-  return <p className="text-center mt-10">Autenticando com Google...</p>;
+export default function Page() {
+  return (
+    <Suspense fallback={<p className="text-center mt-10">Carregando...</p>}>
+      <GoogleCallback />
+    </Suspense>
+  );
 }
